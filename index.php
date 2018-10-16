d329aeddecff4a03907ff195ebcc973f8c15ade0
mcguffin <joern@flyingletters.com>
2018-10-16 16:17:02 +0200

    Release version 0.0.3

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin Name: ACF WP-Objects
 Plugin URI: http://wordpress.org/
 Description: Enter description here.
 Author: Jörn Lund
-Version: 0.0.2
+Version: 0.0.3
 Author URI: https://github.com/mcguffin
 License: GPL3
 Github Repository: mcguffin/acf-wp-objects
