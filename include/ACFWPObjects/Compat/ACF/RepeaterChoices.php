e72ad4190780e97ac93aed221d314ea32d9bafb3
mcguffin <joern@flyingletters.com>
2018-11-13 08:39:04 +0100

    fix nasty mess-up-fields-bug

--- include/ACFWPObjects/Compat/ACF/RepeaterChoices.php
+++ include/ACFWPObjects/Compat/ACF/RepeaterChoices.php
@@ -104,6 +104,7 @@ class RepeaterChoices extends Core\Singleton {
 			'name'			=> 'repeater_value_field',
 			'type'			=> 'select',
 			'ui'			=> 0,
+			'allow_null'	=> 0,
 			'choices'		=> $value_field_choices,
 			'conditions'	=> array(
 				array(
@@ -357,44 +358,44 @@ class RepeaterChoices extends Core\Singleton {
 		selector = '[data-key="repeater_field"] select';
 
 	// reduce value & label field choices when repeater field changes
-	$(document).on( 'change', selector, function(e){
-		if ( !! repeated_fields[ $(this).val() ] ) {
-			var html = '',
-				repeater = $(this).val();
-			/*
-			$(this).closest('.acf-field-settings')
-				.find('[data-key="repeater_label_field"] select,[data-key="repeater_value_field"] select')
-				.each(function(i,el){
-					$(this).find('optgroup').each(function(i,el){
-						if ( $(this).is('[label="'+repeater+'"]') ) {
+	function setupRepeaterChoices( \$this ) {
 
-						}
+		var html = '',
+			repeater = \$this.val();
 
-					})
-				});
-			/*/
-			$.each(repeated_fields[ repeater ],function( val, label ){
-				html += '<option value="'+val+'">' + label + '</option>';
-			});
-			$(this).closest('.acf-field-settings')
-				.find('[data-key="repeater_label_field"] select,[data-key="repeater_value_field"] select')
-				.each(function(i,el){
-					var val = $(this).val(),
-						choiceNull = $(this).find('option[value=""]'),
-						field = acf.getField($(this).closest('.acf-field'));
-
-					$(this).html(html);
-					if ( choiceNull.length ) {
-						$(this).prepend( choiceNull );
-					}
-					$(this).val( val );
-
-				});
-			//*/
-
+		if ( ! repeated_fields[ repeater ] ) {
+			return;
 		}
+
+		$.each( repeated_fields[ repeater ], function( val, label ){
+			html += '<option value="'+val+'">' + label + '</option>';
+		});
+		// set label and value field options
+		\$this.closest('.acf-field-settings')
+			.find('[data-key="repeater_label_field"] select,[data-key="repeater_value_field"] select')
+			.each(function(i,el){
+				//
+				var val = $(this).val(),
+					choiceNull = $(this).find('option[value=""]'),
+					field = acf.getField( $(this).closest('.acf-field') );
+
+				$(this).html(html);
+
+				if ( choiceNull.length ) {
+					$(this).prepend( choiceNull );
+				}
+				$(this).val( val );
+
+			});
+	}
+	$(document).on( 'change', selector, function(e){
+
+		setupRepeaterChoices( $(this) );
+
 	}).ready(function(){
-		$(selector).trigger('change');
+		$(selector).each(function(i,el){
+			setupRepeaterChoices( $(this) );
+		});
 	});
 })(jQuery);
 EOD;
