415a74d0d23b6b1efe924fc1fc085f7c4c08c332
mcguffin <joern@flyingletters.com>
2019-07-11 18:54:23 +0200

    Release 0.1.20 from master

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin Name: ACF WP-Objects
 Plugin URI: http://wordpress.org/
 Description: Connecting WordPress Data with ACF Fields
 Author: Jörn Lund
-Version: 0.1.19
+Version: 0.1.20
 Author URI: https://github.com/mcguffin
 License: GPL3
 Github Repository: mcguffin/acf-wp-objects
