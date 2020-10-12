f169383a8b84cf524c2ced49750dcb3ed650c054
mcguffin <joern@flyingletters.com>
2020-10-12 17:18:35 +0200

    layouts: dont print post_content

--- include/api/page-layouts.php
+++ include/api/page-layouts.php
@@ -12,17 +12,6 @@ function acf_add_page_layout( $args = '' ) {
  *	render layout template
  */
 function acf_page_layouts( $page_layout, $post_id = false ) {
-	if ( ! is_admin() && ACFWPObjects\Compat\ACF\PageLayout::instance()->get( $page_layout, 'save_post_content' ) ) {
-		if ( $content = get_the_content( null, false, $post_id ) ) {
-			$content = wptexturize( $content );
-			$content = prepend_attachment($content);
-			$content = wp_filter_content_tags($content);
-			$content = do_shortcode( $content ); // 11
-			$content = convert_smilies( $content );
-			echo $content;
-			return;
-		};
-	}
 
 	while ( have_rows( $page_layout, $post_id ) ) {
 		the_row();
