6342a17a1726d784316aee3ae5498e19494c2a11
mcguffin <joern@flyingletters.com>
2020-12-11 12:49:58 +0100

    introduce api function acf_is_fieldgroup_admin()

--- include/api/api.php
+++ include/api/api.php
@@ -2,7 +2,22 @@
 
 
 if ( ! function_exists('acf_recreate_field_keys') ) {
+	/**
+	 *	Generate deterministic Field Keys and set them everywhere.
+	 *
+	 *	@param Array $fields ACF Fields including sub_fields
+	 *	@return Array
+	 */
 	function acf_recreate_field_keys( $fields = [] ) {
 		return ACFWPObjects\Compat\ACF\ACF::instance()->recreate_field_keys( $fields );
+	}
+}
+
+if ( ! function_exists('acf_is_fieldgroup_admin') ) {
+	/**
+	 *	@return Boolean
+	 */
+	function acf_is_fieldgroup_admin() {
+		return ACFWPObjects\Compat\ACF\ACF::instance()->is_fieldgroup_admin();
 	}
 }
