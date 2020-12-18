2e80d26207588f3a32757bf6bdd8fcd26ae2595c
joernlund <joern@flyingletters.com>
2020-12-18 14:28:50 +0100

    CSS/JS: Repeater class deny-sort

--- src/js/admin/acf-input/index.js
+++ src/js/admin/acf-input/index.js
@@ -1,3 +1,15 @@
 import 'acf/field-sweet-spot';
 import 'acf/select-conditions';
 import 'acf/id-field';
+
+// repeater field deny sorting
+const RepeaterField = acf.getFieldType('repeater')
+
+// wrap around original addSortable function
+const origSortable = RepeaterField.prototype.addSortable
+RepeaterField.prototype.addSortable = function() {
+	if ( this.$el.hasClass( 'deny-sort') || this.$el.hasClass( 'no-sort') ) {
+		return;
+	}
+	return origSortable.apply( this, arguments );
+}
