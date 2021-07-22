4ab852f67a773976f9948c171b06f2f2c7930770
mcguffin <joern@flyingletters.com>
2021-07-22 19:16:31 +0200

    fix js err with acf free

--- src/js/admin/acf-input/index.js
+++ src/js/admin/acf-input/index.js
@@ -4,12 +4,14 @@ import 'acf/id-field';
 
 // repeater field deny sorting
 const RepeaterField = acf.getFieldType('repeater')
-
-// wrap around original addSortable function
-const origSortable = RepeaterField.prototype.addSortable
-RepeaterField.prototype.addSortable = function() {
-	if ( this.$el.hasClass( 'deny-sort') || this.$el.hasClass( 'no-sort') ) {
-		return;
+if ( RepeaterField ) {
+	// wrap around original addSortable function
+	const origSortable = RepeaterField.prototype.addSortable
+	RepeaterField.prototype.addSortable = function() {
+		if ( this.$el.hasClass( 'deny-sort') || this.$el.hasClass( 'no-sort') ) {
+			return;
+		}
+		return origSortable.apply( this, arguments );
 	}
-	return origSortable.apply( this, arguments );
+
 }
