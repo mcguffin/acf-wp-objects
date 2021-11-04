f6b291b1b8acff52dd1d41f1b84ae2419cc2aed0
mcguffin <joern@flyingletters.com>
2021-11-04 16:48:55 +0100

    open tab: support nested tabs

--- src/js/lib/acf/tab-url-navigation.js
+++ src/js/lib/acf/tab-url-navigation.js
@@ -4,18 +4,28 @@ import $ from 'jquery';
 
 if ( !! document.location.hash ) {
 	const tabTitle = decodeURIComponent(document.location.hash.substr(1));
+	const parentTab = tab => {
+		let $el = tab.$el.parent().closest('.acf-field')
+		while ( ! $el.prevAll('.acf-field-tab:first').length && $el.is('.acf-field') ) {
+			$el = $el.parent().closest('.acf-field')
+		}
+		if ( ! $el.is('.acf-field') ) {
+			return false;
+		}
+		return acf.getField( $el.prevAll('.acf-field-tab:first') )
+	}
 	if ( !! tabTitle ) {
-		acf.addAction('ready_field/type=tab', f => {
+		acf.addAction('ready_field/type=tab', myTab => {
+			let btn, idx, tab = myTab
 			//const $selectedTab = $(`.acf-tab-group a[data-endpoint]:contains("${tabTitle}")`);
-			if ( f.$el.find('.acf-label').text().trim() === tabTitle ) {
-				const btn = $(`li a[data-key="${f.findTab().attr('data-key')}"]`)
-				const idx = btn.closest('ul').find('li').index(btn.closest('li'));
-				console.log(idx)
-				if ( idx !== -1 ) {
-					f.tabs.openTab( f.tabs.tabs[idx] )
-					// @see https://stackoverflow.com/a/52466271/1983694
-					history.replaceState(null, null, ' ');
+			if ( tab.$el.find('.acf-label').text().trim() === tabTitle ) {
+				while ( !! tab && ! myTab.$el.next().is(':visible') ) {
+					btn = $(`li a[data-key="${tab.findTab().attr('data-key')}"]`)
+					idx = btn.closest('ul').find('li').index(btn.closest('li'));
+					tab.tabs.openTab( tab.tabs.tabs[idx] )
+					tab = parentTab( tab )
 				}
+				history.replaceState(null, null, ' ');
 			}
 		});
 	}
