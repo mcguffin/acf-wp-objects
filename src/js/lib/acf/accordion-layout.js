02d70bc9c60fb840b8ba11d7d778870919009c9f
mcguffin <joern@flyingletters.com>
2022-11-23 13:29:06 +0100

    fix accordion collapsed layouts

--- src/js/lib/acf/accordion-layout.js
+++ src/js/lib/acf/accordion-layout.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 
 const doAccordion = $layout => {
 	let $field, field;
+
 	if ( ! $layout.is('.layout') ) {
 		return;
 	}
@@ -23,6 +24,34 @@ const doAccordion = $layout => {
 
 acf.addAction('show', doAccordion )
 
+// acf.addAction('ready', doAccordion )
+
 acf.addAction( 'after_duplicate', ($original,$duplicate) => {
 	setTimeout( () => doAccordion($duplicate), 1 )
 });
+
+const FlexField =
+
+acf.registerFieldType(
+
+	acf.getFieldType('flexible_content').extend({
+		addCollapsed: function() {
+			const pref = acf.getPreference('this.collapsedLayouts')
+			let indices = [],
+				open = false,
+				key = this.get('key');
+
+			if ( !! pref && !! pref[key] ) {
+				indices = pref[key]
+			}
+
+			this.$layouts().each(function (i) {
+				if ( open || ( indices.indexOf(i) > -1 ) ) {
+					$(this).addClass('-collapsed');
+				} else {
+					open = true
+				}
+			});
+		},
+	})
+);
